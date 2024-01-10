64288c55afbf12e3352ffade320cb271f541549f
JOSE CARLOS BARRETO FILHO <85513751+JCARLOS3009@users.noreply.github.com>
2024-01-10 16:06:24 -0300

    Add files via upload

---
+++ converter.py
@@ -0,0 +1,12 @@
+from PIL import Image
+import os
+
+lista_arquivos = os.listdir("imagens")
+
+for arquivo in lista_arquivos:
+    # abrir arquivo
+    imagem = Image.open(f"imagens/{arquivo}").convert("RGB")
+
+    # salvar o arquivo com outro formato
+    imagem.save(f'pdf/{arquivo.replace("png", "pdf")}')
+
